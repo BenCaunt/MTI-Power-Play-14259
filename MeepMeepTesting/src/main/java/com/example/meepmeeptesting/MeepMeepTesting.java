093975a5419ed963dbb44cd091fb64144e9b07d2
Ben Caunt <bdcaunt@ncsu.edu>
2023-06-20 10:45:55 -0400

    potential fix

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -9,17 +10,18 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
+        Pose2d initialPosition = new Pose2d(-36, -60, Math.toRadians(-90));
+        Vector2d secondPosition = new Vector2d(-36, -24);
+        Pose2d finalPosition = new Pose2d(-24, -12, Math.toRadians(180));
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-36, -60, Math.toRadians(-90)))
-                                .lineToLinearHeading(new Pose2d(-36, -2, Math.toRadians(-90)))
-                                .lineToLinearHeading(new Pose2d(-36, -16, Math.toRadians(-190)))
-//                                .lineToLinearHeading(new Pose2d(-11.5, -12, Math.toRadians(-90))) 3
-//                                .lineToLinearHeading(new Pose2d(-36, -12, Math.toRadians(-90))) 2
-//                                .lineToLinearHeading(new Pose2d(-59, -12, Math.toRadians(-180)))
-
+                        drive.trajectorySequenceBuilder(initialPosition)
+                                .setReversed(true)
+                                .splineToConstantHeading(secondPosition, calculateTangent(initialPosition,secondPosition))
+                                .splineToLinearHeading(finalPosition, calculateTangent(secondPosition,finalPosition))
                                 .build()
                 );
 
@@ -28,5 +30,26 @@ public class MeepMeepTesting {
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .start();
+    }
+
+    public static double calculateTangent(Pose2d initialPosition, Pose2d finalPosition) {
+        double xd = initialPosition.getX() - finalPosition.getX();
+        double yd = initialPosition.getY() - finalPosition.getY();
+        return Math.atan2(yd,xd) - Math.PI;
+    }
+    public static double calculateTangent(Vector2d initialPosition, Vector2d finalPosition) {
+        double xd = initialPosition.getX() - finalPosition.getX();
+        double yd = initialPosition.getY() - finalPosition.getY();
+        return Math.atan2(yd,xd) - Math.PI;
+    }
+    public static double calculateTangent(Pose2d initialPosition, Vector2d finalPosition) {
+        double xd = initialPosition.getX() - finalPosition.getX();
+        double yd = initialPosition.getY() - finalPosition.getY();
+        return Math.atan2(yd,xd) - Math.PI;
+    }
+    public static double calculateTangent(Vector2d initialPosition, Pose2d finalPosition) {
+        double xd = initialPosition.getX() - finalPosition.getX();
+        double yd = initialPosition.getY() - finalPosition.getY();
+        return Math.atan2(yd,xd) - Math.PI;
     }
 }
